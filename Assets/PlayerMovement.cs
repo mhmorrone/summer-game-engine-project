bc9ff330f1a463fe0dc1168b1ad1f9dbf4c72ec7
mhmorrone <60272406+mhmorrone@users.noreply.github.com>
2020-06-11 15:22:31 -0400

    playing with unity (created way to control character)

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float runspeed = 40f;
+    float horizontalMove = 0f;
+    float verticalMove = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runspeed;
+        verticalMove = Input.GetAxisRaw("Vertical") * runspeed;
+
+        //Move character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, verticalMove * Time.fixedDeltaTime);
+    }
+
+    //void FixedUpdate()
+    //{
+       // controller.Move(verticalMove * Time.fixedDeltaTime, false, false);
+    //}
+}
