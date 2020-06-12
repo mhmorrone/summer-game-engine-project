885f0514d85255a3d9a6494d4acd4168e1bfd1ec
mhmorrone <60272406+mhmorrone@users.noreply.github.com>
2020-06-12 15:44:41 -0400

    Flips character when moving right or left

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -14,7 +14,7 @@ public class CharacterController : MonoBehaviour
 	//private bool m_Grounded;            // Whether or not the player is grounded.
 	//const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
 	private Rigidbody2D m_Rigidbody2D;
-	//private bool m_FacingRight = true;  // For determining which way the player is currently facing.
+	private bool m_FacingRight = true;  // For determining which way the player is currently facing.
 	private Vector3 m_Velocity = Vector3.zero;
 
 	//[Header("Events")]
@@ -43,13 +43,13 @@ public class CharacterController : MonoBehaviour
 		m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
 
 		// If the input is moving the player right and the player is facing left...
-		if (move > 0 && !m_FacingRight)
+		if (horizontal > 0 && !m_FacingRight)
 		{
 		// ... flip the player.
 		Flip();
 		}
 		// Otherwise if the input is moving the player left and the player is facing right...
-		else if (move < 0 && m_FacingRight)
+		else if (horizontal < 0 && m_FacingRight)
 		{
 		// ... flip the player.
 		Flip();
