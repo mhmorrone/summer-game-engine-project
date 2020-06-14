80dd637ba5c428603bd534e5af428c22edffc967
mhmorrone <60272406+mhmorrone@users.noreply.github.com>
2020-06-14 16:02:15 -0400

    created script for health

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public double maxHealth = 100f;
+    double currentHealth;
+    public double recoverySpeed = 0.05f;
+    public double damageResistance = 1f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentHealth < maxHealth)
+            currentHealth += recoverySpeed * Time.fixedDeltaTime;
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage * damageResistance;
+
+        if (currentHealth <= 0)
+        {
+            //Kill character
+        }
+    }
+}
