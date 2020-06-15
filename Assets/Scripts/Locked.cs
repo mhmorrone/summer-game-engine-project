7114e49c7d3ce4cc4f0214d2f841bd5e071c6379
mhmorrone <60272406+mhmorrone@users.noreply.github.com>
2020-06-15 17:22:45 -0400

    created basic building prefab

---
+++ Assets/Scripts/Locked.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Locked : MonoBehaviour
+{
+    public bool isLocked = false;
+    public Collider2D door;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isLocked)
+        {
+            door.enabled = true;
+        }
+        else
+        {
+            door.enabled = false;
+        }
+
+    }
+}
