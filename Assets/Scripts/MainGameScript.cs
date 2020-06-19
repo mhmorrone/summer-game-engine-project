d07c6340993782e62f3dc1eaf3d7549addf963f8
Christopher Tan <ctan5@stevens.edu>
2020-06-19 15:10:28 -0400

    Added functional pause menu
    Pause has volume option only

---
+++ Assets/Scripts/MainGameScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MainGameScript : MonoBehaviour
+{
+    public Button pauseButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseButton = GameObject.FindGameObjectWithTag("Pause").GetComponent<Button>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.Escape))
+        {
+            print("p key is held down");
+            pauseButton.onClick.Invoke();
+        }
+    }
+}
