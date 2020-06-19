fa8dafceb620a1b003d19cc5e5ce143e58dea52c
Christopher Tan <ctan5@stevens.edu>
2020-06-19 15:27:21 -0400

    Added unpause functionality with P or ESC
    Added unpause functionality with P or ESC

---
+++ Assets/Unpause.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Unpause : MonoBehaviour
+{
+    public Button unPause;
+    // Start is called before the first frame update
+    void Start()
+    {
+        unPause = GameObject.FindGameObjectWithTag("unpause").GetComponent<Button>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.Escape))
+        {
+            print("p key is held down");
+            unPause.onClick.Invoke();
+        }
+    }
+    
+}
