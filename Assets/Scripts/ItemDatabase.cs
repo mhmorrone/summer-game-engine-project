3df489af79c0e8ae5f8670888017d8f0e2880f18
Christopher Tan <ctan5@stevens.edu>
2020-06-25 17:37:21 -0400

    Created an item database script and an item script.
    As we expand the inventory, these scripts will be used.

---
+++ Assets/Scripts/ItemDatabase.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDatabase : MonoBehaviour
+{
+    public List<Item> items = new List<Item>();
+
+    public void Awake()
+    {
+        BuildDatabase();
+    }
+
+    public Item GetItem(int id)
+    {
+        return items.Find(item => item.id == id);
+    }
+
+    void BuildDatabase()
+    {
+        items = new List<Item> { 
+            new Item(0, "Steel Sword", "A sword made of steel.", IronSword,
+            new Dictionary<string, int>
+            {
+                {"Damage", 10 }
+
+            })
+          };
+    }
+}
