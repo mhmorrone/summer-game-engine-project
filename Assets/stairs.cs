b9d3d30bd0672a8d6e65eb7bedffc45a214abfc2
mhmorrone <60272406+mhmorrone@users.noreply.github.com>
2020-06-29 13:58:38 -0400

    Multilevel building prefab and single stair script using variable

---
+++ Assets/stairs.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stairs : MonoBehaviour
+{
+    public GameObject StairsLanding;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        col.gameObject.transform.Translate(StairsLanding.transform.position - col.gameObject.transform.position);
+    }
+}
