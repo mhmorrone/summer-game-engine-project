1a72ce8f08f2f14652adbb4338065c21d5736b03
mhmorrone <60272406+mhmorrone@users.noreply.github.com>
2020-07-01 13:46:13 -0400

    update

---
+++ Assets/OpenCloseDoor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class OpenCloseDoor : MonoBehaviour
+{
+    public Locked locked;
+    public bool open = false;
+    public Transform trans;
+    public GameObject DoorHinge;
+    public float range = 0.5f;
+    public LayerMask playerLayers;
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Collider2D[] players = Physics2D.OverlapCircleAll(trans.position, range, playerLayers);
+            if(players != null)
+            {
+                if (!locked.isLocked)
+                {
+                    open = !open;
+                    if (open)
+                    {
+                        trans.RotateAround(DoorHinge.transform.position, Vector3.forward, -90);
+                    }
+                    else
+                    {
+                        trans.RotateAround(DoorHinge.transform.position, Vector3.forward, 90);
+                    }
+                }
+            }
+            
+        }
+        
+    }
+}
