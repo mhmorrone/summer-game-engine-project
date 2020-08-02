914de7f0577809cc3cb9f5cfb7eb6e44d110a3c5
Christopher Tan <ctan5@stevens.edu>
2020-08-02 19:22:43 -0400

    Updated volume slider in main menu to actually change master audio level

---
+++ Assets/Scripts/OptionsScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class OptionsScript : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    // Start is called before the first frame update
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+}
