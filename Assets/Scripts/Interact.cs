5ea89d22a0651a1fdffa5fcd7aa491036c5b887d
Aaron <aaronalfar02@hotmail.com>
2020-08-02 17:26:27 -0400

    Dialogue

---
+++ Assets/Scripts/Interact.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact : MonoBehaviour
+{
+    public Rigidbody2D Player;
+    public Rigidbody2D NPC;
+    public bool dialogueStarted = false;
+    public DialogueTrigger trigger;
+    public DialogueManager dm;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Vector3.Distance(Player.transform.position, NPC.transform.position) < 10)
+        {
+            if ((Input.GetKeyDown("e") || Input.GetKeyDown("E")) && dialogueStarted == false)
+            {
+                trigger.TriggerDialogue();
+                dialogueStarted = true;
+
+            }
+            else if ((Input.GetKeyDown("e") || Input.GetKeyDown("E")) && dialogueStarted == true)
+            {
+                dm.DisplayNextSentence();
+            }
+
+        }
+        
+    }
+}
