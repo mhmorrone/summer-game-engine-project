6f3341b0ff4a640b63fbb60f5390f411a7a6390a
eelcaz <zlee@stevens.edu>
2020-08-09 21:59:03 -0400

    Health Bar Update (still not functioning)

---
+++ Assets/HealthBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+
+    public Slider slider;
+    
+    public void setMaxHealth(double health)
+    {
+        slider.maxValue = (float) health;
+        slider.value = (float) health;
+    }
+
+    public void setHealth(double health)
+    {
+        slider.value = (float) health;
+    }
+
+}
