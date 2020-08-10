3fbcc05715b08f811dc1f62072129144f9f0e487
TheShicksinator <shicks@stevens.edu>
2020-08-10 01:27:36 -0700

    Moved health bar into proper position and added game over screen

---
+++ Assets/Scripts/GameOverMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverMenu : MonoBehaviour
+{
+    public Button quit;
+
+    void Start()
+    {
+
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
