94bdbb276a2fc0ac94eed0d9afce80e9700a9dfa
mhmorrone <mhmorrone98@gmail.com>
2020-08-11 16:06:47 -0400

    fixed lagging, attack range, and game boundaries

--- Assets/Scripts/Spawn_NPCs.cs
+++ Assets/Scripts/Spawn_NPCs.cs
@@ -8,6 +8,7 @@ public class Spawn_NPCs : MonoBehaviour
     //The NPC character prefabs
     public GameObject NPC1;
     public GameObject NPC2;
+    public GameObject player;
     //boundaries of the game map
     public Transform topBound;
     public Transform bottomBound;
@@ -20,7 +21,8 @@ public class Spawn_NPCs : MonoBehaviour
     {
         //a random amount of NPCs are spawned into the game at the start to fill the map
         //it is the same amount of the female and male NPCs
-        int num_people = 2 * UnityEngine.Random.Range(100, 500); //determines number of NPCs to be spawned
+        int num_people = 2 * UnityEngine.Random.Range(20, 50); //determines number of NPCs to be spawned throughout map
+        //number of NPCs spawned is kept low to prevent lagging
         for (int i = 0; i < num_people; i += 2)
         {
             //Determines random position within the map and spawns an NPC there
@@ -29,6 +31,18 @@ public class Spawn_NPCs : MonoBehaviour
             Instantiate(NPC1, new Vector2(Horizontal, Vertical), topBound.rotation);
             Horizontal = UnityEngine.Random.Range(leftBound.position.x, rightBound.position.x);
             Vertical = UnityEngine.Random.Range(bottomBound.position.y, topBound.position.y);
+            Instantiate(NPC2, new Vector2(Horizontal, Vertical), topBound.rotation);
+        }
+        num_people = 2 * UnityEngine.Random.Range(20, 50);//determines number of NPCs to be spawned throughout map
+        //this is done so that the player immediately sees NPCs and the map feels fuller
+        for (int i = 0; i < num_people; i += 2)
+        {
+            //Determines random position within the map and spawns an NPC there
+            Horizontal = UnityEngine.Random.Range(player.transform.position.x - 40, player.transform.position.x + 40);
+            Vertical = UnityEngine.Random.Range(player.transform.position.y - 40, player.transform.position.y + 40);
+            Instantiate(NPC1, new Vector2(Horizontal, Vertical), topBound.rotation);
+            Horizontal = UnityEngine.Random.Range(player.transform.position.x - 40, player.transform.position.x + 40);
+            Vertical = UnityEngine.Random.Range(player.transform.position.y - 40, player.transform.position.y + 40);
             Instantiate(NPC2, new Vector2(Horizontal, Vertical), topBound.rotation);
         }
     }
--- Assets/Scripts/Spawn_Zombies.cs
+++ Assets/Scripts/Spawn_Zombies.cs
@@ -10,6 +10,7 @@ public class Spawn_Zombies : MonoBehaviour
     public int difficulty = 3;
     public GameObject zombie1;
     public GameObject zombie2;
+    public GameObject player;
     //Game map boundaries
     public Transform topBound;
     public Transform bottomBound;
@@ -23,7 +24,8 @@ public class Spawn_Zombies : MonoBehaviour
     {
         //a random amount of zombies are spawned into the game at the start to fill the map
         //it is the same amount of the female and male zombies
-        int num_zombies = difficulty * 2 * UnityEngine.Random.Range(10, 50);//determines number of zombies to be spawned
+        //the number of zombies is kept lower to prevent lagging
+        int num_zombies = difficulty * 2 * UnityEngine.Random.Range(1, 10);//determines number of zombies to be spawned throughout the board
         for (int i = 0; i < num_zombies; i += 2)
         {
             //Determines random position within the map and spawns a zombie there
@@ -32,6 +34,18 @@ public class Spawn_Zombies : MonoBehaviour
             Instantiate(zombie1, new Vector2(Horizontal, Vertical), topBound.rotation);
             Horizontal = UnityEngine.Random.Range(leftBound.position.x, rightBound.position.x);
             Vertical = UnityEngine.Random.Range(bottomBound.position.y, topBound.position.y);
+            Instantiate(zombie2, new Vector2(Horizontal, Vertical), topBound.rotation);
+        }
+        num_zombies = difficulty * 2 * UnityEngine.Random.Range(1, 10);//determines number of zombies to be spawned near the player
+        //this is done so that the player immediately finds zombies and the map feels fuller
+        for (int i = 0; i < num_zombies; i += 2)
+        {
+            //Determines random position within the map and spawns a zombie there
+            Horizontal = UnityEngine.Random.Range(player.transform.position.x - 40, player.transform.position.x + 40);
+            Vertical = UnityEngine.Random.Range(player.transform.position.y - 40, player.transform.position.y + 40);
+            Instantiate(zombie1, new Vector2(Horizontal, Vertical), topBound.rotation);
+            Horizontal = UnityEngine.Random.Range(player.transform.position.x - 40, player.transform.position.x + 40);
+            Vertical = UnityEngine.Random.Range(player.transform.position.y - 40, player.transform.position.y + 40);
             Instantiate(zombie2, new Vector2(Horizontal, Vertical), topBound.rotation);
         }
     }
